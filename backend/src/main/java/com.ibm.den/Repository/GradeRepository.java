0b089a6104e5f50792ff0f34674c845b794be614
Nuta-Bogdan <nuta_bogda@yahoo.com>
2023-05-01 20:40:40 +0300

    Fixed error regarding the Repository interfaces

--- backend/src/main/java/com.ibm.den/Repository/GradeRepository.java
+++ backend/src/main/java/com.ibm.den/Repository/GradeRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GradeRepository extends JpaRepository<Grade, Id> {
+public interface GradeRepository extends JpaRepository<Grade, Integer> {
 }
--- backend/src/main/java/com.ibm.den/Repository/MentorRepository.java
+++ backend/src/main/java/com.ibm.den/Repository/MentorRepository.java
@@ -4,5 +4,5 @@ import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface MentorRepository extends JpaRepository<Mentor, Id> {
+public interface MentorRepository extends JpaRepository<Mentor, Integer> {
 }
