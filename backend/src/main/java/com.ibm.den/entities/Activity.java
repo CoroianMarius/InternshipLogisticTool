f9497d21580dbcfe78c63670c74f5e99c6f260ec
Nuta-Bogdan <nuta_bogda@yahoo.com>
2023-05-23 12:13:05 +0300

    Added getters and setters

--- backend/src/main/java/com.ibm.den/entities/Activity.java
+++ backend/src/main/java/com.ibm.den/entities/Activity.java
@@ -3,27 +3,76 @@ package com.ibm.den.entities;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "activity")
 public class Activity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "activity_id")
-    private int id;
+    @Column
+    private long id;
 
     @Column
     private String name;
 
     @Column
-    @OneToMany()
-    @JoinColumn(name = "task_id")
-    private ArrayList<Task> tasks;
+    @OneToMany(mappedBy = "id")
+    private List<Task> tasks;
 
     @Column
-    @OneToMany()
-    @JoinColumn(name = "student_id")
-    private ArrayList<Student> students;
+    @OneToMany(mappedBy = "id")
+    private List<Team> teams;
+
+    public Activity(String name) {
+        this.name = name;
+    }
+
+    public Activity(long id, String name, List<Task> tasks, List<Team> teams) {
+        this.id = id;
+        this.name = name;
+        this.tasks = tasks;
+        this.teams = teams;
+    }
+
+    public Activity() {
+    }
 
 
+    public void addTeam(Team team) {
+        teams.add(team);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
 }
