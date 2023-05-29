0c9d2ce62125a12d7ccac3805a34812890be3110
Nuta-Bogdan <nuta_bogda@yahoo.com>
2023-05-30 01:54:56 +0300

    Added method to findByName

--- backend/src/main/java/com.ibm.den/repository/MentorRepository.java
+++ backend/src/main/java/com.ibm.den/repository/MentorRepository.java
@@ -13,4 +13,5 @@ public interface MentorRepository extends JpaRepository<Mentor, Long> {
 
     public Mentor findByNameAndPassword(String email,String password);
 
+    Mentor findByName(String mentor);
 }
