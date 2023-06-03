bbc10d8cb2fdbbbc4448137cc06a93dc9f73e20a
Nuta-Bogdan <nuta_bogda@yahoo.com>
2023-06-03 21:22:02 +0300

    Added @Repository annotation

--- backend/src/main/java/com.ibm.den/repository/AttendanceRepository.java
+++ backend/src/main/java/com.ibm.den/repository/AttendanceRepository.java
@@ -3,9 +3,10 @@ package com.ibm.den.repository;
 import com.ibm.den.entities.Attendance;
 import com.ibm.den.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-
+@Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     ArrayList<Attendance> findByStudent(Student student);
 }
