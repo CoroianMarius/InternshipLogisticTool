aa86f8643da51c3eb6465702b082a2b8edf15f99
CovercaElena <elena.coverca02@e-uvt.ro>
2023-06-03 19:12:11 +0300

    Working on Attendence service

---
+++ InternshipLogistic/frontend/src/app/services/view-attendence.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Student } from "../model/Student";
+import { Attendance } from "../model/Attendance";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ViewAttendenceService {
+
+  constructor(private http: HttpClient) {}
+
+  ViewAttendence(self: Student) {
+    return this.http.get<{ [key: string]: Attendance }>('http://localhost:8080/api/attendance/list' + self.email)
+      .pipe(
+        map((res) => {
+          const attendances = [];
+          for (const key in res) {
+            if (res.hasOwnProperty(key)) {
+              attendances.push({ ...res[key], id: key });
+            }
+          }
+          return attendances;
+        })
+      );
+  }
+}
--- InternshipLogistic/frontend/src/app/view-attendence/view-attendence.component.ts
+++ InternshipLogistic/frontend/src/app/view-attendence/view-attendence.component.ts
@@ -1,12 +1,33 @@
-import {Component} from '@angular/core';
-import {Students} from "../../Students";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { map } from "rxjs/operators";
+import { Student } from "../model/Student";
+import { ViewAttendenceService } from "../services/view-attendence.service";
 
 @Component({
   selector: 'app-view-attendence',
   templateUrl: './view-attendence.component.html',
   styleUrls: ['./view-attendence.component.css']
 })
-export class ViewAttendenceComponent {
+export class ViewAttendenceComponent implements OnInit {
+  selectedStudent = new Student("Student20", "email20", false);
+  attendances: any[] = [];
 
+  constructor(private http: HttpClient, private viewAttendenceService: ViewAttendenceService) {
+  }
 
+  ngOnInit() {
+    this.viewAttendence();
+  }
+
+  private viewAttendence() {
+    this.viewAttendenceService.ViewAttendence(this.selectedStudent)
+      .pipe(
+        map((data) => data.filter((attendance) => Object.keys(attendance).length > 1))
+      )
+      .subscribe((attendances) => {
+        console.log(attendances);
+        this.attendances = attendances;
+      });
+  }
 }
