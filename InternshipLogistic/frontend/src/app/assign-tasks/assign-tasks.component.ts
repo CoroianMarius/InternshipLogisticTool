2fa7203a775204bfaefcf2aefd94a6171b1acb92
CovercaElena <elena.coverca02@e-uvt.ro>
2023-06-06 06:28:45 +0300

    working

--- InternshipLogistic/frontend/src/app/assign-tasks/assign-tasks.component.ts
+++ InternshipLogistic/frontend/src/app/assign-tasks/assign-tasks.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import {STUDENTS} from "../student_lists";
 import {Student} from "../model/Student";
 import {MatInputModule} from '@angular/material/input';
@@ -6,16 +6,18 @@ import {NgFor} from '@angular/common';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormControl, FormsModule} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import {NgForm} from "@angular/forms";
+import { ViewTeamService } from '../services/view-team.service';
+import {Team} from "../model/Team";
+import { ChartType, ChartOptions } from 'chart.js';
+import {Grade} from "../model/Grade";
+import {ViewGradesTeamService} from "../services/view-grades-team.service";
+import { AuthService } from '../services/auth.service';
+import {map, Subject} from 'rxjs';
+import {Router} from "@angular/router";
 
-interface Food {
-  value: string;
-  viewValue: string;
-}
 
-interface Car {
-  value: string;
-  viewValue: string;
-}
 
 @Component({
   selector: 'app-assign-tasks',
@@ -25,19 +27,71 @@ interface Car {
 export class AssignTasksComponent {
   selectedValue = "";
   selectedCar = "";
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
-  ];
+  selectedMentor: Student;
+  allLeaders: Student[] = [];
+  allStudents:Student[]=[];
 
-  cars: Car[] = [
-    {value: 'volvo', viewValue: 'Volvo'},
-    {value: 'saab', viewValue: 'Saab'},
-    {value: 'mercedes', viewValue: 'Mercedes'},
-  ];
+selectedLeader!:Student;
 
-  toppings = new FormControl('');
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  selectedStudents = new FormControl([]);
 
+
+
+  constructor(private http: HttpClient, private authService: AuthService, private router: Router, private ViewTeamService: ViewTeamService)
+  {
+    this.selectedMentor = this.authService.getLoggedInUser();
+  }
+
+
+
+  ngOnInit() {
+    this.viewTeamLeaders();
+  }
+
+  private viewTeamLeaders() {
+    this.ViewTeamService.ViewTeamLeaders().subscribe((leaders) => {
+      console.log(leaders);
+      this.allLeaders = leaders;
+    });
+  }
+
+  onSelectLeader(leader: Student) {
+    this.selectedLeader = leader;
+
+    this.http
+      .get<any>('http://localhost:8080/api/team/' + leader.email)
+      .pipe(
+        map((res) => {
+          const team = [];
+          for (const key in res) {
+            if (key !== 'id') {
+              const student = { ...res[key], id: key };
+              delete student.id; // Exclude the 'id' property
+              team.push(student);
+            }
+          }
+          return team;
+        })
+      )
+      .subscribe((team) => {
+        console.log(team[1]);
+        const studentObject = team[1];
+        this.allStudents = Object.values(studentObject);
+      });
+  }
+  addTask(form:NgForm)
+  {
+    if (form.valid) {
+
+      const taskName = form.controls['taskName'].value;
+      const leader = form.controls['leader'].value;
+      const students = form.value.students;
+
+      // Extract the emails from the selected students
+      console.log(taskName);
+      console.log(leader);
+      console.log(students);
+    }
+
+  }
 }
